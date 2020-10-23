efa5903e9cd8adca8b650da7e88641c9e56cbb63
Adrian Anaya <aanaya79@cnm.edu>
2020-10-23 12:14:05 -0600

    Implemented many-to-many

--- src/main/java/edu/cnm/deepdive/codebreaker/model/entity/User.java
+++ src/main/java/edu/cnm/deepdive/codebreaker/model/entity/User.java
@@ -1,17 +1,25 @@
 package edu.cnm.deepdive.codebreaker.model.entity;
 
 import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.UUID;
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Index;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+import javax.persistence.OrderBy;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.GenericGenerator;
+import org.springframework.core.annotation.Order;
 import org.springframework.lang.NonNull;
 
 @SuppressWarnings("JpaDataSourceORMInspection")
@@ -53,6 +61,22 @@ public class User {
   private String displayName;
 
   @NonNull
+  @OneToMany(fetch = FetchType.LAZY, mappedBy = "originator", cascade = CascadeType.ALL, orphanRemoval = true) //same as field; orphan is match taken out of list
+  @OrderBy("started DESC")
+  private final List<Match> matchesOriginated = new LinkedList<>(); // originator_id
+
+  @NonNull
+  @OneToMany(fetch = FetchType.LAZY, mappedBy = "winner", cascade =
+      {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
+  @OrderBy("deadline DESC")
+  private final List<Match> matchesWon = new LinkedList<>();
+
+  @NonNull
+  @ManyToMany(mappedBy = "players", fetch = FetchType.LAZY) // users can play a lot of games and dont want them all every time
+  @OrderBy("deadline DESC")
+  private final List<Match> matches = new LinkedList<>();
+
+  @NonNull
   public UUID getId() {
     return id;
   }
@@ -87,5 +111,20 @@ public class User {
 
   public void setDisplayName(@NonNull String displayName) {
     this.displayName = displayName;
+  }
+
+  @NonNull
+  public List<Match> getMatchesOriginated() {
+    return matchesOriginated;
+  }
+
+  @NonNull
+  public List<Match> getMatchesWon() {
+    return matchesWon;
+  }
+
+  @NonNull
+  public List<Match> getMatches() {
+    return matches;
   }
 }
